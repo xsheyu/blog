52a326d697cdf090942219a9d548523a6b72768e
xsheyu <1334280479@qq.com>
2016-12-26 18:32:19 +0800

    me页面

---
+++ blog/js/me.js
@@ -0,0 +1,12 @@
+/**
+ * Created by 鱼鱼 on 2016/12/26.
+ */
+window.onload=function  () {
+    var aside_basic=document.getElementById("aside_basic");
+    var aside_detail=document.getElementById("aside_detail");
+    var basic1=document.getElementById("basic");
+    var detail1=document.getElementById("detail");
+    basic.onclick=function () {
+        basic1.setid("")
+    }
+};
